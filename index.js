766b105723f5e7b2a9d724be20fa0d43c7d836ae
ElyosCaesar <ElyosCaesar@DESKTOP-7AUGHBB>
2019-10-24 02:39:34 +0330

    put complited

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 //imports and system initial
 const express = require("express");
+var bodyParser = require("body-parser");
 var inside = require("point-in-polygon");
 const fs = require("fs");
 const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 //local variables
 let allPolygons = [];
@@ -10,6 +17,7 @@ let allPolygons = [];
 //--------------------------------------------------
 //APIs
 
+//get
 app.get("/", (request, response) => {
   response.send("hello world");
 });
@@ -17,7 +25,6 @@ app.get("/gis/testpoint/:lat/:long", (request, response) => {
   // response.send(`lat : ${request.params.lat} , long : ${request.params.long}`);
   const polygons = [];
   allPolygons.forEach(element => {
-    console.log(element.geometry);
     if (
       inside(
         [parseFloat(request.params.lat), parseFloat(request.params.long)],
@@ -30,8 +37,10 @@ app.get("/gis/testpoint/:lat/:long", (request, response) => {
   response.send({ polygons: polygons });
 });
 
+//put
 app.put("/gis/addpolygon", (request, response) => {
-  response.send("hello wold");
+  allPolygons.push(request.body);
+  response.send(allPolygons);
 });
 
 //--------------------------------------------------
